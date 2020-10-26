8265cd3ff9b74d171bf093ae2108d0f7b22ac8e2
di.qin <di.qin@thoughtworks.com>
2020-10-26 08:16:41 +0800

    [qd]: finish p2.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,27 +1,21 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.WebApplicationContext;
 
 @RestController
-@Scope("singleton")
 public class GreetingController {
 
     private final GreetingService greetingService;
-    private final WebApplicationContext webApplicationContext;
 
-    public GreetingController(GreetingService greetingService, WebApplicationContext webApplicationContext) {
+    public GreetingController(GreetingService greetingService) {
         System.out.println("Controller has been called.");
         this.greetingService = greetingService;
-        this.webApplicationContext = webApplicationContext;
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return webApplicationContext.getBean(GreetingService.class).sayHi();
+        return new GreetingService().sayHi();
     }
 
 }
